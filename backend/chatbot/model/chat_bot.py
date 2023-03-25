b3c5bacc0ea5daca51761fd788ea083947ead875
Atharva Swami <atharvaswami@gmail.com>
2023-03-25 16:43:00 +0530

    hello endpoint

--- backend/chatbot/model/chat_bot.py
+++ backend/chatbot/model/chat_bot.py
@@ -174,10 +174,6 @@ def get_issue(chk_dis):
 
             disease_input=cnf_dis[conf_inp]
             return disease_input
-            # print("Did you mean: ",cnf_dis,"?(yes/no) :",end="")
-            # conf_inp = input("")
-            # if(conf_inp=="yes"):
-            #     break
         else:
             print("Enter valid symptom.")
 
@@ -186,7 +182,6 @@ def get_sickdays():
         try:
             num_days=int(input("Okay. From how many days ? : "))
             return num_days
-            break
         except:
             print("Enter valid input.")
 
@@ -241,36 +236,7 @@ def tree_to_code(tree, feature_names):
     chk_dis=",".join(feature_names).split(",")
     symptoms_present = []
     disease_input=get_issue(chk_dis) 
-    # while True:
-
-    #     print("\nEnter the symptom you are experiencing  \t\t",end="->")
-    #     disease_input = input("")
-    #     conf,cnf_dis=check_pattern(chk_dis,disease_input)
-    #     if conf==1:
-    #         print("searches related to input: ")
-    #         for num,it in enumerate(cnf_dis):
-    #             print(num,")",it)
-    #         if num!=0:
-    #             print(f"Select the one you meant (0 - {num}):  ", end="")
-    #             conf_inp = int(input(""))
-    #         else:
-    #             conf_inp=0
-
-    #         disease_input=cnf_dis[conf_inp]
-    #         break
-    #         # print("Did you mean: ",cnf_dis,"?(yes/no) :",end="")
-    #         # conf_inp = input("")
-    #         # if(conf_inp=="yes"):
-    #         #     break
-    #     else:
-    #         print("Enter valid symptom.")
     num_days=get_sickdays()
-    # while True:
-    #     try:
-    #         num_days=int(input("Okay. From how many days ? : "))
-    #         break
-    #     except:
-    #         print("Enter valid input.")
     def recurse(node, depth):
         indent = "  " * depth
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
@@ -288,51 +254,10 @@ def tree_to_code(tree, feature_names):
                 recurse(tree_.children_right[node], depth + 1)
         else:
             present_disease = print_disease(tree_.value[node])
-            # print( "You may have " +  present_disease )
             red_cols = reduced_data.columns 
             symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
             symp_res=get_symptoms(symptoms_given)
-            # dis_list=list(symptoms_present)
-            # if len(dis_list)!=0:
-            #     print("symptoms present  " + str(list(symptoms_present)))
-            # print("symptoms given "  +  str(list(symptoms_given)) )
-            # print("Are you experiencing any ")
-            # symptoms_exp=[]
-            # for syms in list(symptoms_given):
-            #     inp=""
-            #     print(syms,"? : ",end='')
-            #     while True:
-            #         inp=input("")
-            #         if(inp=="yes" or inp=="no"):
-            #             break
-            #         else:
-            #             print("provide proper answers i.e. (yes/no) : ",end="")
-            #     if(inp=="yes"):
-            #         symptoms_exp.append(syms)
             get_result(symp_res,num_days,present_disease)
-            # second_prediction=sec_predict(symptoms_exp)
-            # # print(second_prediction)
-            # calc_condition(symptoms_exp,num_days)
-            # if(present_disease[0]==second_prediction[0]):
-            #     print("You may have ", present_disease[0])
-            #     print(description_list[present_disease[0]])
-
-            #     # readn(f"You may have {present_disease[0]}")
-            #     # readn(f"{description_list[present_disease[0]]}")
-
-            # else:
-            #     print("You may have ", present_disease[0], "or ", second_prediction[0])
-            #     print(description_list[present_disease[0]])
-            #     print(description_list[second_prediction[0]])
-
-            # # print(description_list[present_disease[0]])
-            # precution_list=precautionDictionary[present_disease[0]]
-            # print("Take following measures : ")
-            # for  i,j in enumerate(precution_list):
-            #     print(i+1,")",j)
-
-            # # confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
-            # # print("confidence level is " + str(confidence_level))
 
     recurse(0, 1)
 getSeverityDict()
